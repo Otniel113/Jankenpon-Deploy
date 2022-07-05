b056562469043de366c479986a36625a81af0014
Otniel113 <otniel.abiezer.saragih@gmail.com>
2022-07-05 13:04:59 +0700

    Menambah battle

--- app.py
+++ app.py
@@ -4,10 +4,39 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from tensorflow import expand_dims
 import numpy as np
 import os
+import random
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
 model = load_model('best_model.h5')
+
+def enemy_choice():
+    choice = random.randint(0,2)
+    if (choice == 0):
+        return "Batu"
+    elif (choice == 1):
+        return "Gunting"
+    else:
+        return "Kertas"
+
+def battle_condition(yours, enemy):
+    if (yours == enemy):
+        return "Seri!"
+    elif (yours == "Gunting"):
+        if (enemy == "Kertas"):
+            return "Anda menang!"
+        else:
+            return "Anda kalah!"
+    elif (yours == "Kertas"):
+        if (enemy == "Gunting"):
+            return "Anda kalah!"
+        else:
+            return "Anda menang!"
+    else:
+        if (enemy == "Kertas"):
+            return "Anda kalah!"
+        else:
+            return "Anda menang!"
 
 def convert_prediction(predicted):
     if (predicted[0] == 1):
@@ -35,7 +64,9 @@ def index():
             img_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(img_path)
             prediction = predict_label(img_path)
-            return render_template('index.html', uploaded_image=image.filename, prediction=prediction)
+            enemy = enemy_choice()
+            battle = battle_condition(prediction, enemy)
+            return render_template('index.html', uploaded_image=image.filename, prediction=prediction, enemy=enemy, battle=battle)
 
     return render_template('index.html')
 
